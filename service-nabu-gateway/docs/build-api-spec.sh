eb0dabab5f03ac4eb522e9d3cfe225c1b9b4f6f2
Giuseppe Cafarelli <gcafarelli@blockchain.com>
2024-04-04 18:13:01 +0200

    update script paths

--- service-nabu-gateway/docs/build-api-spec.sh
+++ service-nabu-gateway/docs/build-api-spec.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-npx @redocly/cli bundle ./api.yml --lint --output ./generated/openapi --skip-rule no-ambiguous-paths || true
-npx @redocly/cli build-docs ./api.yml --output ./generated/index.html
+npx @redocly/cli bundle ./$SCRIPT_DIR/api.yml --lint --output ./$SCRIPT_DIR/generated/openapi --skip-rule no-ambiguous-paths || true
+npx @redocly/cli build-docs ./$SCRIPT_DIR/api.yml --output ./$SCRIPT_DIR/generated/index.html
 open file://$SCRIPT_DIR/generated/index.html
