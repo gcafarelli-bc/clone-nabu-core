0c7c8f958e4e6501e8995eb3805c577fdf3dff2e
Giuseppe Cafarelli <gcafarelli@blockchain.com>
2024-04-04 18:27:51 +0200

    fix path in the script for build-docs

--- service-nabu-gateway/docs/build-api-spec.sh
+++ service-nabu-gateway/docs/build-api-spec.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 npx @redocly/cli lint $SCRIPT_DIR/api.yml
 npx @redocly/cli bundle $SCRIPT_DIR/api.yml --output $SCRIPT_DIR/generated/openapi
 #npx @redocly/cli skip-rule $SCRIPT_DIR/api.yml no-ambiguous-paths
-npx @redocly/cli build-docs ./$SCRIPT_DIR/api.yml --output $SCRIPT_DIR/generated/index.html
+npx @redocly/cli build-docs $SCRIPT_DIR/api.yml --output $SCRIPT_DIR/generated/index.html
