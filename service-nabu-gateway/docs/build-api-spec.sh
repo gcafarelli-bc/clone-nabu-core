7afd58c50c9c292d1dc0a8242d3111dcf942ce22
Giuseppe Cafarelli <gcafarelli@blockchain.com>
2024-04-04 18:22:56 +0200

    fix skip-rule

--- service-nabu-gateway/docs/build-api-spec.sh
+++ service-nabu-gateway/docs/build-api-spec.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 npx @redocly/cli lint $SCRIPT_DIR/api.yml
-npx @redocly/cli bundle $SCRIPT_DIR/api.yml --output $SCRIPT_DIR/generated/openapi no-ambiguous-paths
-npx @redocly/cli skip-rule $SCRIPT_DIR/api.yml
+npx @redocly/cli bundle $SCRIPT_DIR/api.yml --output $SCRIPT_DIR/generated/openapi
+npx @redocly/cli skip-rule $SCRIPT_DIR/api.yml no-ambiguous-paths
 npx @redocly/cli build-docs ./$SCRIPT_DIR/api.yml --output $SCRIPT_DIR/generated/index.html
