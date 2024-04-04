394988b086491914c502e8a304515e639801bc5e
Giuseppe Cafarelli <gcafarelli@blockchain.com>
2024-04-04 18:16:39 +0200

    refactor build-api-spec.sh script

--- service-nabu-gateway/docs/build-api-spec.sh
+++ service-nabu-gateway/docs/build-api-spec.sh
@@ -4,7 +4,6 @@ set -euxo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-npx @redocly/cli lint ./$SCRIPT_DIR/api.yml
-npx @redocly/cli bundle ./$SCRIPT_DIR/api.yml --output ./$SCRIPT_DIR/generated/openapi --skip-rule no-ambiguous-paths || true
-npx @redocly/cli build-docs ./$SCRIPT_DIR/api.yml --output ./$SCRIPT_DIR/generated/index.html
-open file://$SCRIPT_DIR/generated/index.html
+npx @redocly/cli lint $SCRIPT_DIR/api.yml
+npx @redocly/cli bundle $SCRIPT_DIR/api.yml --output $SCRIPT_DIR/generated/openapi --skip-rule no-ambiguous-paths || true
+npx @redocly/cli build-docs ./$SCRIPT_DIR/api.yml --output $SCRIPT_DIR/generated/index.html
